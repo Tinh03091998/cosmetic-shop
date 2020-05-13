8722b24187edc24d48a169b97590f8dc93dc9914
TinhNT1998 <tinhnt@2-b.io>
2020-05-14 00:31:16 +0700

    add files in migration folder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password', 50);
             $table->integer('role_id')->unsigned();
-            //$table->foreign('role_id')->references('id')->on('roles');
+            $table->foreign('role_id')->references('id')->on('roles');
             $table->rememberToken();
             $table->timestamps();
         });
